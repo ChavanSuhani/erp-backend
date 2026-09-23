f2bd6a6ea1de1c7ab57ce80e1e93f4d45e649f7f
Divesh Adivarekar <100069544+diveshadivarekar@users.noreply.github.com>
2026-09-23 15:10:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -100,6 +100,7 @@ app.use((err, req, res, next) => {
 ========================= */
 
 // const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {
@@ -110,7 +111,6 @@ const startServer = async () => {
 
     console.log("Database connected. Starting HTTP server...");
 
-    const PORT = process.env.PORT || 5000;
 
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Backend running on port ${PORT}`);
